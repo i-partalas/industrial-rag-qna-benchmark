e3fe34dc669ea0b48fbde8f9a412775a9e47b253
Ioannis Partalas <partalas.ioan@gmail.com>
2024-10-29 23:52:43 +0100

    Remove deepeval dependency

--- src/utils/suppress_warnings.py
+++ src/utils/suppress_warnings.py
@@ -1,17 +1,8 @@
 import logging
 import os
-import warnings
 
 # Suppress TensorFlow info and warnings
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
-
-# Suppress DeepEval UserWarning
-warnings.filterwarnings(
-    "ignore",
-    category=UserWarning,
-    message="You are using deepeval version",
-    module="deepeval",
-)
 
 # Silence logger warnings from the Unstructured module
 logging.getLogger("unstructured").setLevel(logging.ERROR)
