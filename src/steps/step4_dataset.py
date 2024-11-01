f4e7f9b10f98f67b34363292eae3398c4076fc84
Ioannis Partalas <partalas.ioan@gmail.com>
2024-11-01 23:58:52 +0100

    Functionality adjustments in step4_dataset.py

--- src/steps/step4_dataset.py
+++ src/steps/step4_dataset.py
@@ -2,41 +2,34 @@ import streamlit as st
 
 
 def display(prev_step, next_step):
-    st.header("Upload or Create an Evaluation Dataset")
+    st.header("Upload or Create a Test Dataset")
 
-    method_1 = "Upload your own evaluation dataset"
-    method_2 = "Generate a synthetic evaluation dataset based on uploaded PDF file(s)"
+    method_1 = "Upload your own test dataset"
+    method_2 = "Generate a synthetic test dataset based on uploaded PDF file(s)"
     choice = st.radio(
-        label="Choose the method to proceed with the evaluation dataset:",
+        label="Choose the method to proceed with the test dataset:",
         options=(method_1, method_2),
-        # TODO: Complete the help msg
-        help="For the structure and format of the file, please advise ...",
+        help="Ensure the mandatory columns 'question' and 'ground_truth' are included exactly as here specified.",
     )
 
-    dataset_uploaded = False
+    testset_choice = False
     if choice == method_1:
-        uploaded_file = st.file_uploader(
-            "Upload your evaluation dataset file:", type="xlsx"
+        uploaded_testset = st.file_uploader(
+            "Upload your test dataset file:", type="xlsx"
         )
-        if uploaded_file:
-            st.success(f"Uploaded your evaluation dataset: {uploaded_file.name}")
-            dataset_uploaded = True
+        if uploaded_testset:
+            testset_choice = True
+            st.session_state.uploaded_testset = uploaded_testset
+            st.success(f"Uploaded test dataset: {uploaded_testset.name}")
     elif choice == method_2:
-        uploaded_pdfs = st.file_uploader(
-            "Upload your PDF file(s):", type="pdf", accept_multiple_files=True
-        )
-        if uploaded_pdfs:
-            st.success(
-                f"Uploaded {len(uploaded_pdfs)} PDF file(s) for synthetic dataset generation"
-            )
-            dataset_uploaded = True
+        testset_choice = True
 
     button_footers = st.columns([1, 1, 8])
     button_footers[0].button("Back", on_click=prev_step)
 
     button_footers[1].button("Next", on_click=next_step)
-    # if dataset_uploaded:
+    # if testset_choice:
     #     button_footers[1].button("Next", on_click=next_step)
     # else:
-    #     st.warning("Please upload a dataset to proceed.")
+    #     st.warning("Please select one of the available options.")
     #     button_footers[1].button("Next", on_click=next_step, disabled=True)
